af13dc567f0ecbd002a704f1ea6a5aaaa1bbe736
ja-zg <edjber@hotmail.com>
2013-10-28 22:26:21 +0100

    adding helper

---
+++ HyperactiveApp.java
@@ -0,0 +1,10 @@
+package hr.android.jsonparsing;
+
+public class HyperactiveApp {
+	protected static String URL = "http://androidtest.apiary.io/get-users";
+	protected static String USERS = "users";
+	protected static String NAME = "name";
+	protected static String LAST_NAME = "last_name";
+	protected static String IMAGE = "image";
+	protected static String NUMBER = "number";
+}
